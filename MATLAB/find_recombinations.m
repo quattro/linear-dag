40140c81cc160241dc691f3dbbc8dd51f92bc58e
Luke Jen O'Connor <loconnor@broadinstitute.org>
2024-02-05 13:33:41 -0500

    Improve runtime of find_recombinations
    Row lookup tables now use dictionaries for faster removal of elements

--- MATLAB/find_recombinations.m
+++ MATLAB/find_recombinations.m
@@ -14,25 +14,28 @@ edgeList = zeros(nnz(A),3);
 % Build the triolist
 nTrio = 0;
 nEdge = 0;
+B = A';
 for child = 1:n
-    [~, rowParents, rowWeights] = find(A(child,:));
+    [rowParents, ~, rowWeights] = find(B(:,child));
     if ~isempty(rowWeights)
         [~, ~, weightInd] = unique(rowWeights);
         weight_sets = index_array(weightInd);
         % weight_sets = {find(rowWeights < 0) find(rowWeights > 0)}
         for kk = 1:length(weight_sets)
             if ~isempty(weight_sets{kk})
-                these_parents = rowParents(weight_sets{kk});
+                rows = cell2mat(weight_sets{kk}.keys);
+                these_parents = rowParents(rows);
                 if length(these_parents) > 1
                     for pp = 2:length(these_parents)
                         nTrio = nTrio+1;
 
                         % Need to modify triolist so that it keeps track of both weights
-                        trioList(nTrio,:) = [these_parents(pp-1) these_parents(pp) child rowWeights(weight_sets{kk}(pp)) ];
+                        trioList(nTrio,:) = [these_parents(pp-1) these_parents(pp) ...
+                            child rowWeights(rows(pp)) ];
                     end
                 else
                     nEdge = nEdge+1;
-                    edgeList(nEdge,:) = [these_parents child rowWeights(weight_sets{kk})];
+                    edgeList(nEdge,:) = [these_parents child rowWeights(rows)];
                 end
             end
         end
@@ -51,7 +54,11 @@ parentRows{1} = index_array(trioList(:,1), n);
 parentRows{2} = index_array(trioList(:,2), n);
 childRows = index_array(trioList(:,3), n);
 cliqueRows = index_array(trioList(:,5), nClique);
-cliqueRows(end+1:size(cliques,1)) = {[]};
+cliqueRows(end+1:size(cliques,1)) = {containers.Map};
+parentRows{1}(end+1:size(cliques,1)) = {containers.Map};
+parentRows{2}(end+1:size(cliques,1)) = {containers.Map};
+childRows(end+1:size(cliques,1)) = {containers.Map};
+
 cliqueSize = cellfun(@length,cliqueRows);
 
 newNode = n;
@@ -60,7 +67,7 @@ while true
     loopcounter = loopcounter + 1;
     [nr, c] = max(cliqueSize);
 
-    rows = cliqueRows{c};
+    rows = cell2mat(cliqueRows{c}.keys);
     rowParents = cliques(c,:);
     rowChildren = trioList(rows, 3);
     rowWeights = trioList(rows, 4);
@@ -71,12 +78,12 @@ while true
     end
 
     % Remove these rows from lookup tables
-    parentRows{1}{rowParents(1)} = setdiff(parentRows{1}{rowParents(1)}, rows);
-    parentRows{2}{rowParents(2)} = setdiff(parentRows{2}{rowParents(2)}, rows);
+    parentRows{1}{rowParents(1)} = remove(parentRows{1}{rowParents(1)}, num2cell(rows));
+    parentRows{2}{rowParents(2)} = remove(parentRows{2}{rowParents(2)}, num2cell(rows));
     for ch = 1:nr
-        childRows{rowChildren(ch)} = setdiff(childRows{rowChildren(ch)}, rows);
+        childRows{rowChildren(ch)} = remove(childRows{rowChildren(ch)}, rows(ch));
     end
-    cliqueRows{c} = [];
+    cliqueRows{c} = containers.Map;
     cliqueSize(c) = 0;
     
     % Introduce a new node
@@ -89,7 +96,7 @@ while true
     nEdge = nEdge+2;
 
     % Connect newNode with any child that now has only one parent
-    nParentPairs = cellfun(@(rows,weight)sum(trioList(rows,4)==weight),...
+    nParentPairs = cellfun(@(rows,weight)sum(trioList(cell2mat(rows.keys),4)==weight),...
         childRows(rowChildren),num2cell(rowWeights));
     ch = find(nParentPairs == 0);
     edgeList(nEdge+1:nEdge+length(ch),1) = newNode;
@@ -109,7 +116,7 @@ end
 
 % Put trivial cliques into the edgelist
 for child = 1:n
-    rows = childRows{child};
+    rows = cell2mat(childRows{child}.keys);
     if isempty(rows)
         continue;
     end
@@ -130,13 +137,14 @@ A = sparse(edgeList(:,2),edgeList(:,1),edgeList(:,3),newNode,newNode);
 % Returns a cell array of length max(idx) [or n] such that all(idx(cells{j}) == j)
     function cells = index_array(idx,n)
         if isempty(idx)
-            cells = {};
+            cells = containers.Map;
             return;
         end
         if ~exist('n')
             n = max(idx);
         end
-        cells = accumarray(idx,(1:length(idx))',[n 1],@(x){x});
+        cells = accumarray(idx,(1:length(idx))',[n 1],@(x){containers.Map(x,true(size(x)))});
+        cells(cellfun(@isempty,cells)) = repmat({containers.Map},sum(cellfun(@isempty,cells)),1);
     end
 
 % Updates rows of the trio list of which one of its parents (depending
@@ -145,21 +153,30 @@ A = sparse(edgeList(:,2),edgeList(:,1),edgeList(:,3),newNode,newNode);
 % updated accordingly, the number of cliques is also updated. Returns
 % the new number of cliques.
     function nClique = update_trioList(whichParent, parent, children, newNode, nClique)
-        whichRows = intersect(vertcat(childRows{children}), ...
-            parentRows{whichParent}{parent});
-        parentRows{whichParent}{parent} = setdiff(parentRows{whichParent}{parent}, whichRows);
+        children_rows = cellfun(@(D){cell2mat(D.keys)},childRows(children));
+        children_rows = [children_rows{:}];
+        parent_rows = cell2mat(parentRows{whichParent}{parent}.keys);
+        whichRows = intersect(children_rows,parent_rows);
+        if isempty(whichRows)
+            return
+        end
+        parentRows{whichParent}{parent} = remove(parentRows{whichParent}{parent}, num2cell(whichRows));
         trioList(whichRows, whichParent) = newNode;
-        parentRows{whichParent}{newNode} = whichRows;
+        parentRows{whichParent}{newNode} = containers.Map(whichRows,true(size(whichRows)));
 
         [affected_cliques, ~, which_affected_cliques] = unique(trioList(whichRows,5));
         for wc = 1:length(affected_cliques)
-            cliqueRows{affected_cliques(wc)} = setdiff(cliqueRows{affected_cliques(wc)}, ...
-                whichRows);
-            cliqueSize(affected_cliques(wc)) = length(cliqueRows{affected_cliques(wc)});
-            cliqueRows{nClique + 1} = whichRows(which_affected_cliques == wc);
+            affected_rows = whichRows(which_affected_cliques == wc);
+            if ~isempty(cliqueRows{affected_cliques(wc)})
+                cliqueRows{affected_cliques(wc)} = remove(cliqueRows{affected_cliques(wc)}, ...
+                    num2cell(affected_rows));
+                cliqueSize(affected_cliques(wc)) = length(cliqueRows{affected_cliques(wc)});
+            end
+            cliqueRows{nClique + 1} = containers.Map(affected_rows,...
+                true(size(affected_rows)));
             cliqueSize(nClique + 1) = length(cliqueRows{nClique + 1});
-            cliques(nClique + 1, :) = trioList(cliqueRows{nClique + 1}(1), 1:2);
-            trioList(cliqueRows{nClique + 1},5) = nClique + 1;
+            cliques(nClique + 1, :) = trioList(affected_rows(1), 1:2);
+            trioList(affected_rows,5) = nClique + 1;
             nClique = nClique + 1;
         end
     end
