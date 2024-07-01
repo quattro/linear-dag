5e0ef0669d5f5a12438c7b5541238298feeeef2e
Luke Jen O'Connor <loconnor@broadinstitute.org>
2024-07-01 16:13:35 -0400

    Update __init__.py

--- src/linear_dag/__init__.py
+++ src/linear_dag/__init__.py
@@ -3,29 +3,42 @@
 # SPDX-License-Identifier: MIT
 from importlib.metadata import PackageNotFoundError, version  # pragma: no cover
 
+from .brick_graph import (
+    BrickGraph as BrickGraph,
+)
+from .data_structures import (
+    CountingArray as CountingArray,
+    DiGraph as DiGraph,
+    LinkedListArray as LinkedListArray,
+    ModHeap as ModHeap,
+    Queue as Queue,
+    Stack as Stack,
+)
+from .genotype_processing import (
+    apply_maf_threshold as apply_maf_threshold,
+    binarize as binarize,
+    compute_af as compute_af,
+    flip_alleles as flip_alleles,
+)
+
+# annoying 'as' notation to avoid warnings/errors about unused imports...
 from .linarg_workflow import (
     run_linarg_workflow as run_linarg_workflow,
+)
+from .linear_arg_inference import (
+    add_samples_to_linear_arg as add_samples_to_linear_arg,
+    add_singleton_variants as add_singleton_variants,
+    infer_brick_graph_using_containment as infer_brick_graph_using_containment,
+    linearize_brick_graph as linearize_brick_graph,
 )
 from .lineararg import (
     LinearARG as LinearARG,
 )
-
-# from .intervals import (
-#     Intervals as Intervals,
-# )
-# annoying 'as' notation to avoid warnings/errors about unused imports...
 from .one_summed import (
     compute_path_sums as compute_path_sums,
     construct_1_summed_DAG_fast as construct_1_summed_DAG_fast,
     construct_1_summed_DAG_slow as construct_1_summed_DAG_slow,
 )
-from .pathsumdag import (
-    PathSumDAG as PathSumDAG,
-)
-
-# from .pathsumdag_old import (
-#     PathSumDAGTesting as PathSumDAGTesting,
-# )
 from .simulate import (
     Simulate as Simulate,
 )
@@ -33,14 +46,7 @@ from .solve import (
     spinv_triangular as spinv_triangular,
 )
 from .trios import (
-    LinkedListArray as LinkedListArray,
     Trios as Trios,
-)
-from .utils import (
-    apply_maf_threshold as apply_maf_threshold,
-    binarize as binarize,
-    compute_af as compute_af,
-    flip_alleles as flip_alleles,
 )
 
 
