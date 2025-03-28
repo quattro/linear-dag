5f18fea5ea5da8051ae06ce230e5145aec96e0b6
amberzshen <amberzshen@gmail.com>
2025-03-28 10:38:27 -0400

    comment out metadata and parallel_processing from init

--- src/linear_dag/core/__init__.py
+++ src/linear_dag/core/__init__.py
@@ -2,8 +2,8 @@ from .brick_graph import BrickGraph as BrickGraph
 from .linear_arg_inference import linear_arg_from_genotypes as linear_arg_from_genotypes
 from .lineararg import LinearARG as LinearARG
 from .recombination import Recombination as Recombination
-from .metadata import read_metadata as read_metadata
-from .parallel_processing import ParallelOperator as ParallelOperator
+# from .metadata import read_metadata as read_metadata
+# from .parallel_processing import ParallelOperator as ParallelOperator
 from .sample_info import SampleInfo as SampleInfo
 from .solve import (
     spinv_triangular as spinv_triangular,
