e209d2e6350e50a5a532f2702a757ae85b4eacc7
Luke Jen O'Connor <loconnor@broadinstitute.org>
2025-04-23 10:01:41 -0400

    simulation.py support multiple traits

--- src/linear_dag/association/simulation.py
+++ src/linear_dag/association/simulation.py
@@ -1,15 +1,20 @@
 import numpy as np
 from ..core import LinearARG
-
+from typing import Optional
+from numpy.random import Generator
 
 def simulate_phenotype(linarg: LinearARG, heritability: float, 
-                        alpha: float = 0, fraction_causal: float = 1, return_genetic_component: bool = False):
+                        alpha: float = 0, fraction_causal: float = 1, 
+                        num_traits: int = 1,
+                        return_genetic_component: bool = False,
+                        seed: Optional[Generator] = None):
     """
     Simulates quantitative phenotypes
         y = X * beta + epsilon
     with specified heritability, AF-dependent architecture, and polygenicity
     :param linarg: genotypes
     :param heritability: between 0 and 1
+    :param num_traits: number of phenotypes to simulate
     :param alpha: between -1 and 0; cf. Schoech et al 2019 Nat Commun
     :param fraction_causal: between 0 and 1
     :return: vector of phenotype values
@@ -17,18 +22,19 @@ def simulate_phenotype(linarg: LinearARG, heritability: float,
 
     N, M = linarg.shape
 
-    beta = np.random.randn(M)
+    beta = seed.standard_normal((M, num_traits), dtype=np.float32)
     heterozygosity = 2 * linarg.allele_frequencies * (1 - linarg.allele_frequencies)
     beta[heterozygosity == 0] = 0
     heterozygosity[heterozygosity == 0] = 1
-    beta = beta * np.sqrt(heterozygosity**alpha)
+    beta = beta * np.sqrt(heterozygosity**alpha).reshape(-1, 1)
     is_causal = np.random.rand(M) < fraction_causal
-    beta = beta * is_causal
+    beta = beta * is_causal.reshape(-1, 1)
 
     y_bar = linarg @ beta
-    y_bar -= np.mean(y_bar)
-    y_bar /= np.std(y_bar)
+    y_bar -= np.mean(y_bar, axis=0)
+    y_bar /= np.std(y_bar, axis=0)
     y_bar *= np.sqrt(heritability)
-    y = y_bar + np.random.randn(N) * np.sqrt(1 - heritability)
+    y = y_bar + np.random.randn(N, num_traits) * np.sqrt(1 - heritability)
+    y = y.astype(np.float32)
 
     return y, y_bar if return_genetic_component else y
