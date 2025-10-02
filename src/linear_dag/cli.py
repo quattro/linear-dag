3bd81c9038f94fa21b58a2cedace8b94a54b5472
amberzshen <amberzshen@gmail.com>
2025-10-02 19:10:02 -0400

    remove shutdown

--- src/linear_dag/cli.py
+++ src/linear_dag/cli.py
@@ -243,9 +243,7 @@ def _prs(args):
     result = pl.DataFrame(frame_dict)
             
     logger.info("Writing results")
-    result.write_csv(f"{args.out}.tsv", separator="\t")
-    logger.info("Done!")
-    
+    result.write_csv(f"{args.out}.tsv", separator="\t")    
     logger.info(f"Finished in {time.time() - t:.2f} seconds")
     
     return
@@ -315,8 +313,8 @@ def _assoc_scan(args):
                 logger=logger,
                 in_place_op=True,
             )
-        genotypes.shutdown()
-        logger.info("Shut down parallel operator")
+        # genotypes.shutdown()
+        # logger.info("Shut down parallel operator")
 
         # If variant info was requested, await its loading
         if vinfo_future is not None:
