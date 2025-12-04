75c56ee2c246a362c155a49be1fc6c63faac1514
Luke Jen O'Connor <loconnor@broadinstitute.org>
2025-12-04 11:39:58 -0500

    Create hatch_build.py

---
+++ hatch_build.py
@@ -0,0 +1,18 @@
+import os
+
+import scipy
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        build_data["include-dirs"] = [os.path.dirname(scipy.__file__)]
+
+
+def get_include_dirs():
+    return [os.path.dirname(scipy.__file__)]
+
+
+def build_hook(config):
+    config["include-dirs"] = get_include_dirs()
