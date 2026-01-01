b58cdb36025443c26c6faa6bddb3822f1881f9d9
amberzshen <amberzshen@gmail.com>
2026-01-01 15:32:02 -0500

    remove print statments from util

--- src/linear_dag/association/util.py
+++ src/linear_dag/association/util.py
@@ -148,19 +148,13 @@ def get_genotype_variance_explained_recompute_AC(
         denominator *= -2
         # avoid large memory allocation
         num_snps = XtCD.shape[0]
-        
-        
-        print(f'num_homozygotes shape: {num_homozygotes.shape}')
-        
+                
         for start_idx in range(0, num_snps, batch_size):
             
             end_idx = min(start_idx + batch_size, num_snps)
 
             batch_homozygotes = num_homozygotes[start_idx:end_idx]
             batch_allele_counts = allele_counts[start_idx:end_idx]
-            
-            print(f'batch_homozygotes shape: {batch_homozygotes.shape}')
-            print(f'batch_allele_counts shape: {batch_allele_counts.shape}')
 
             denominator[start_idx:end_idx] += (
                 batch_allele_counts
