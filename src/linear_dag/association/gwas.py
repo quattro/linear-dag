888b2c36a8ff8c75b631c4d1156ae5b735d2dffd
amberzshen <amberzshen@gmail.com>
2026-01-08 18:30:23 -0500

    fix indexing

--- src/linear_dag/association/gwas.py
+++ src/linear_dag/association/gwas.py
@@ -75,9 +75,9 @@ def get_gwas_beta_se(
     beta = Xty[:, :num_traits]
     
     print('Xty:')
-    print(f' 4134685: {beta[4134685+85278662]}')
-    print(f' 5487797: {beta[5487797+85278662]}')
-    print(f' 7811929: {beta[7811929+85278662]}')
+    print(f' 89413347: {beta[89413347]}')
+    print(f' 90766459: {beta[90766459]}')
+    print(f' 93090591: {beta[93090591]}')
     
     # beta *= (right_op.shape[0]/num_nonmissing)
     assert np.shares_memory(beta, Xty)
@@ -100,19 +100,19 @@ def get_gwas_beta_se(
         )
         
     print('denominator:')
-    print(f' 4134685: {denominator[4134685+85278662]}')
-    print(f' 5487797: {denominator[5487797+85278662]}')
-    print(f' 7811929: {denominator[7811929+85278662]}')
+    print(f' 89413347: {denominator[89413347]}')
+    print(f' 90766459: {denominator[90766459]}')
+    print(f' 93090591: {denominator[93090591]}')
     
     print('allele_counts:')
-    print(f' 4134685: {allele_counts[4134685+85278662]}')
-    print(f' 5487797: {allele_counts[5487797+85278662]}')
-    print(f' 7811929: {allele_counts[7811929+85278662]}')
+    print(f' 89413347: {allele_counts[89413347]}')
+    print(f' 90766459: {allele_counts[90766459]}')
+    print(f' 93090591: {allele_counts[93090591]}')
     
     print('beta:')
-    print(f' 4134685: {beta[4134685+85278662]}')
-    print(f' 5487797: {beta[5487797+85278662]}')
-    print(f' 7811929: {beta[7811929+85278662]}')
+    print(f' 89413347: {beta[89413347]}')
+    print(f' 90766459: {beta[90766459]}')
+    print(f' 93090591: {beta[93090591]}')
 
     # avoid numerical issues for variants with no variance
     numerically_zero = 1e-4 # typical nonzero values > 1
--- src/linear_dag/association/util.py
+++ src/linear_dag/association/util.py
@@ -190,14 +190,14 @@ def _get_genotype_variance(
     var_genotypes = 3 * allele_counts - 2 * carrier_counts  # 4 * num_homozygotes + num_heterozygotes
     
     print('var_genotypes:')
-    print(f' 4134685: {var_genotypes[4134685+85278662]}')
-    print(f' 5487797: {var_genotypes[5487797+85278662]}')
-    print(f' 7811929: {var_genotypes[7811929+85278662]}')
+    print(f' 89413347: {var_genotypes[89413347]}')
+    print(f' 90766459: {var_genotypes[90766459]}')
+    print(f' 93090591: {var_genotypes[93090591]}')
     
     print('carrier_counts:')
-    print(f' 4134685: {carrier_counts[4134685+85278662]}')
-    print(f' 5487797: {carrier_counts[5487797+85278662]}')
-    print(f' 7811929: {carrier_counts[7811929+85278662]}')
+    print(f' 89413347: {carrier_counts[89413347]}')
+    print(f' 90766459: {carrier_counts[90766459]}')
+    print(f' 93090591: {carrier_counts[93090591]}')
     
     return var_genotypes, carrier_counts
 
